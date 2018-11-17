ab5bb61bbb892bef7e2efad2d25d084e557cf381
karimsoliman94 <karimsoliman94@hotmail.com>
2018-11-17 01:34:40 -0800

    init hw3

---
+++ csharpHW/hw3/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace hw3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var trainDataPath = @"../../conll03_ner/eng.train.small";
+
+            train(trainDataPath);
+
+
+        }
+
+        public static void train(string filePath){
+
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                var list = new List<Sentence>();
+                while (!reader.EndOfStream)
+                {
+                    var currentSentence = new Sentence();
+                    var line = reader.ReadLine();
+                    while(!string.IsNullOrWhiteSpace(line))
+                    {
+                        //we have line for word
+                        var word = new Word();
+
+                        var lineSplit = line.Split(' ');
+                        word
+                        //fill in word object
+                        line = reader.ReadLine();
+                    }
+                    list.Add(currentSentence);
+                }
+            }
+        }
+    }
+
+    public class Word
+    {
+        public string text = "";
+        public string postTag = "";
+        public string chunkTag = "";
+        public string truth = "";
+    }
+
+    public class Sentence
+    {
+        public List<Word> words = new List<Word>();
+    }
+}
