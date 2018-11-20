9615b32e134ebf4d7cc177298af95adc7fd5a308
karimsoliman94 <karimsoliman94@hotmail.com>
2018-11-19 22:51:38 -0800

    done

--- csharpHW/hw3/Program.cs
+++ csharpHW/hw3/Program.cs
@@ -15,26 +15,27 @@ namespace hw3
 
         public static void Main(string[] args)
         {
-            var start = DateTime.Now;
-
-            var trainDataPath = @"../../conll03_ner/eng.train.small";
-            var trainDataResult = @"../../conll03_ner/eng.train.small.result";
-
-            var devDataPath = @"../../conll03_ner/eng.dev.small";
-            var devDataResult = @"../../conll03_ner/eng.dev.small.result";
-
-            var testDataPath = @"../../conll03_ner/eng.test.small";
-            var testDataResult = @"../../conll03_ner/eng.test.small.result";
-
-            getFeatureClassesFromFile(trainDataPath, out HashSet<string> posSet, out HashSet<string> wordSet, out HashSet<string> chunkSet, out HashSet<string> nerTagSet);
-
-
-            var initialWeights = initializeWeights(nerTagSet, posSet, wordSet, chunkSet);
-
-            var trainedWeights = train(trainDataPath, numIterations, initialWeights, nerTagSet);
 
             if (args.Length == 1)
             {
+                var start = DateTime.Now;
+
+                var trainDataPath = @"../../conll03_ner/eng.train.small";
+                var trainDataResult = @"../../conll03_ner/eng.train.small.result";
+
+                var devDataPath = @"../../conll03_ner/eng.dev.small";
+                var devDataResult = @"../../conll03_ner/eng.dev.small.result";
+
+                var testDataPath = @"../../conll03_ner/eng.test.small";
+                var testDataResult = @"../../conll03_ner/eng.test.small.result";
+
+                getFeatureClassesFromFile(trainDataPath, out HashSet<string> posSet, out HashSet<string> wordSet, out HashSet<string> chunkSet, out HashSet<string> nerTagSet);
+
+
+                var initialWeights = initializeWeights(nerTagSet, posSet, wordSet, chunkSet);
+
+                var trainedWeights = train(trainDataPath, numIterations, initialWeights, nerTagSet);
+
                 switch (args[0])
                 {
                     case "train":
@@ -48,18 +49,20 @@ namespace hw3
                         break;
 
                 }
+                var end = DateTime.Now;
+
+                Console.WriteLine("Total time taken: " + (end - start));
+
             }
             else
             {
                 Console.WriteLine("To run this code, provide parameter train, dev, or test, to determine which set to run on");
                 Console.WriteLine("This will always train on small training set and run on the chosen small set.");
             }
-
-            var end = DateTime.Now;
-
-            Console.WriteLine("Total time taken: " + (end - start));
         }
+          
 
+        //read through the file and get posTag set, chunk set, ner tag set, and word set
         private static void getFeatureClassesFromFile(string filePath, out HashSet<string> posSet, out HashSet<string> wordSet, out HashSet<string> chunkSet, out HashSet<string> nerTagSet)
         {
             posSet = new HashSet<string>();
@@ -83,8 +86,6 @@ namespace hw3
                         nerTagSet.Add(linesplit[3]);
                         line = reader.ReadLine();
                     }
-
-
                 }
             }
 
@@ -154,9 +155,9 @@ namespace hw3
 
         }
 
+
         public static Dictionary<string, double> train(string filePath, int numIterations, Dictionary<string, double> initialWeights, HashSet<string> tagset)
         {
-
             var sentenceList = new List<Sentence>();
             using (StreamReader reader = new StreamReader(filePath))
             {
@@ -230,9 +231,7 @@ namespace hw3
 
                     }
 
-
-                    //might need to check if the vectors are equal before adding and avg
-                    //update weights
+                    //update weights if vectors are not the same 
                     if (!sameVector(predictedFeatureVector, truthFeatureVector))
                     {
                         currentWeights = addVectorsWeight(currentWeights, truthFeatureVector);
@@ -250,6 +249,7 @@ namespace hw3
 
         }
 
+        //check if 2 vectors are the same. double comparison is ok since we start with same weight vector and add integers from features
         private static bool sameVector(Dictionary<string, double> predictedFeatureVector, Dictionary<string, double> truthFeatureVector)
         {
             if (predictedFeatureVector.Count != truthFeatureVector.Count)
@@ -278,6 +278,7 @@ namespace hw3
 
         }
 
+        //used for getting the avg weight vector, divide each item in the dictionary with the denominator
         private static Dictionary<string, double> divideVector(Dictionary<string, double> vector, int denominator)
         {
             var finalDiv = new Dictionary<string, double>();
@@ -289,30 +290,17 @@ namespace hw3
             return finalDiv;
         }
 
+        //we assume the first item is the weight vector, we only need to loop through smaller list to add it
         private static Dictionary<string, double> addVectorsWeight(Dictionary<string, double> mainVector, Dictionary<string, double> secondVector)
         {
-            //var finalSum = new Dictionary<string, double>();
-            //foreach (var feature in mainVector)
-            //{
-            //    var key = feature.Key;
-            //    var value = feature.Value;
-            //    var secondValue = secondVector.ContainsKey(key) ? secondVector[key] : 0.0;
-
-            //    finalSum.Add(key, value + secondValue);
-            //}
-
             foreach (var secondFeature in secondVector)
             {
                 mainVector[secondFeature.Key] += secondFeature.Value;
-
-                //if (!finalSum.ContainsKey(secondFeature.Key))
-                //{
-                //    finalSum.Add(secondFeature.Key, secondFeature.Value);
-                //}
             }
             return mainVector;
         }
 
+        //add 2 vecotrs where one isnt the weight. need to make sure items in each list are represented
         private static Dictionary<string, double> addVectors(Dictionary<string, double> mainVector, Dictionary<string, double> secondVector)
         {
             var finalSum = new Dictionary<string, double>();
@@ -336,35 +324,21 @@ namespace hw3
             return finalSum;
         }
 
+        //we assume that the main vector is a weight vector and so we only need to subract the second vector
         private static Dictionary<string, double> subtractVectors(Dictionary<string, double> mainVector, Dictionary<string, double> secondVector)
         {
-            //var finalDiff = new Dictionary<string, double>();
-            //foreach (var feature in mainVector)
-            //{
-            //    var key = feature.Key;
-            //    var value = feature.Value;
-            //    var secondValue = secondVector.ContainsKey(key) ? secondVector[key] : 0.0;
-
-            //    finalDiff.Add(key, value - secondValue);
-            //}
-
             foreach (var secondFeature in secondVector)
             {
                 mainVector[secondFeature.Key] -= secondFeature.Value;
-                //if (!finalDiff.ContainsKey(secondFeature.Key))
-                //{
-                //    finalDiff.Add(secondFeature.Key, -1.0 * secondFeature.Value);
-                //}
+
             }
-            //return finalDiff;
             return mainVector;
         }
 
+        //sets the appropriate features to 1 if they are present
         public static Dictionary<string, double> getFeatures(Sentence sentence, int wordNumber, string currentTag, string previousTag)
         {
             Dictionary<string, double> featureVector = new Dictionary<string, double>();
-
-            //add first is upper and not first word
 
             //handle stop
             if (currentTag.Equals(stopWord))
@@ -375,7 +349,6 @@ namespace hw3
 
             var currentWord = sentence.words[wordNumber];
 
-            //i think these will always be same since truth and predicted are calculated by me
             //first letter is uppercase
             if (char.IsUpper(currentWord.text[0]))
             {
@@ -388,26 +361,20 @@ namespace hw3
                 featureVector.Add("allUpper" + currentTag, 1);
             }
 
-
             //current tag
             featureVector.Add("NER#" + currentTag, 1);
 
             //bigram of prediction class 
             featureVector.Add(previousTag + "#" + currentTag, 1);
 
-            ////pos tag feature
-            //featureVector.Add("TAG#"+currentWord.posTag, 1);
-
-            ////chunk tag feature
-            //featureVector.Add("CHUNK#"+currentWord.chunkTag, 1);
-
-            //word feature
-            // featureVector.Add(currentTag+"#"+currentWord.text, 1);
+            // word feature
+            featureVector.Add(currentTag+"#"+currentWord.text, 1);
 
             return featureVector;
 
         }
 
+        //sets our initial weight vector
         private static Dictionary<string, double> initializeWeights(HashSet<string> tagset, HashSet<string> posSet, HashSet<string> wordSet, HashSet<string> chunkSet)
         {
 
@@ -418,57 +385,28 @@ namespace hw3
                 foreach (var inside in tagset)
                 {
                     //add all bigrams
-                    weights.Add(outside + "#" + inside, Rand());
+                    weights.Add(outside + "#" + inside, rand.NextDouble());
                 }
 
-                weights.Add("firstUpper" + outside, Rand());
-                weights.Add("allUpper" + outside, Rand());
-
-
+                weights.Add("firstUpper" + outside, rand.NextDouble());
+                weights.Add("allUpper" + outside, rand.NextDouble());
 
                 //add start-tag bigrams
-                weights.Add(startWord + "#" + outside, Rand());
+                weights.Add(startWord + "#" + outside, rand.NextDouble());
                 //add tag-stop bigrams
-                weights.Add(outside + "#" + stopWord, Rand());
+                weights.Add(outside + "#" + stopWord, rand.NextDouble());
                 //add unigram tag
-                weights.Add("NER#" + outside, Rand());
+                weights.Add("NER#" + outside, rand.NextDouble());
 
                 //WORD CLASS FEATURE
-                //foreach (var word in wordSet)
-                //{
-                //    weights.Add(outside+"#" + word, Rand());
-                //}
+                foreach (var word in wordSet)
+                {
+                    weights.Add(outside+"#" + word, rand.NextDouble());
+                }
             }
 
-
-
-            ////POS TAG CLASS FEATURE
-            //foreach(var tag in posSet)
-            //{
-            //    weights.Add("TAG#"+tag, Rand());
-            //}
-
-
-
-
-
-            ////CHUNK TAG CLASS FEATURE
-
-            //foreach(var chunk in chunkSet)
-            //{
-            //    weights.Add("CHUNK#"+chunk, Rand());
-            //}
-
-
             return weights;
-
         }
-
-        public static double Rand()//between -1 AND 1
-        {
-            return (rand.NextDouble()); //* 2) - 1;
-        }
-
 
         public static List<string> Viterbi(Sentence sentence, Dictionary<String, double> weights, HashSet<string> stateSet)
         {
@@ -566,21 +504,7 @@ namespace hw3
                 currentTag = bestTag[currentTag];
             }
 
-
             return tags;
-
-
-            //var outobj = new List<List<string>>();
-
-            //for (int m = 1; m <= dataObject.Count; m++)
-            //{
-            //    var tuple = new List<string>();
-            //    tuple.Add(dataObject[m - 1][0]);
-            //    tuple.Add(tags[m].Split(' ')[1]);
-            //    outobj.Add(tuple);
-            //}
-            //var json = JsonConvert.SerializeObject(outobj);
-            //writer.WriteLine(json);
         }
 
         private static double dotProduct(Dictionary<string, double> mainv1, Dictionary<string, double> v2)
